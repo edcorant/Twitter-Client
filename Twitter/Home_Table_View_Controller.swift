fffb05c4dfdc9812932a9a5fa99ff3bacfb98ab0
Eduardo Antonini <eduardoantonini@icloud.com>
2020-09-21 21:10:49 -0400

    Implemented part 9. Golden Master Candidate 1.

--- Twitter/Home_Table_View_Controller.swift
+++ Twitter/Home_Table_View_Controller.swift
@@ -27,8 +27,9 @@ class Home_Table_View_Controller: UITableViewController {
     
     @objc func load_tweets() {
         
+        num_tweets = 20
         let feed_url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let params = ["count" : 20]
+        let params = ["count" : num_tweets]
         
         TwitterAPICaller.client?.getDictionariesRequest(url: feed_url, parameters: params, success: { (tweets : [NSDictionary]) in
             
@@ -49,6 +50,7 @@ class Home_Table_View_Controller: UITableViewController {
         })
     }
     
+    
     @IBAction func Sign_out_button(_ sender: Any) {
         // logout from api
         TwitterAPICaller.client?.logout()
@@ -63,9 +65,9 @@ class Home_Table_View_Controller: UITableViewController {
         
         // create cell object
         let cell = tableView.dequeueReusableCell(withIdentifier: "Tweet_Cell", for: indexPath) as! Tweet_Cell_View
-        
+        // create 'user' dictionary (necessary for user details)
         let user = twitter_feed[indexPath.row]["user"] as! NSDictionary
-        
+        // extract url of profile photo from user dictionary
         let photo_url = URL(string: user["profile_image_url_https"] as! String)
         
         // populate cell object
@@ -81,6 +83,37 @@ class Home_Table_View_Controller: UITableViewController {
         return cell
     }
     
+    func get_sum_moar_tweets () {
+        
+        num_tweets += 20
+        let feed_url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let params = ["count" : num_tweets]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: feed_url, parameters: params, success: { (tweets : [NSDictionary]) in
+            
+            // empty feed container
+            self.twitter_feed.removeAll()
+            // repopulate container
+            for tweet in tweets {
+                self.twitter_feed.append(tweet)
+            }
+            
+            // update the feed
+            self.tableView.reloadData()
+            
+        }, failure: { (Error) in
+            print("Failed to retrieve tweets.")
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        // this is triggered when the user scrolls all the way to the bottom of the feed
+        if indexPath.row + 1 == twitter_feed.count {
+            get_sum_moar_tweets()
+        }
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -92,6 +125,22 @@ class Home_Table_View_Controller: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return twitter_feed.count
     }
-
+    
+    // MARK: - Miscellaneous
+    
+    /*
+     the two functions below are used to set the status bar elements, such as time and battery status,
+     to a bright white color in order to better contrast with the dark navigation bar.
+     
+     referenced from: https://www.ioscreator.com/tutorials/change-color-status-bar-ios-tutorial
+     */
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        navigationController?.navigationBar.barStyle = .black
+    }
 
 }
